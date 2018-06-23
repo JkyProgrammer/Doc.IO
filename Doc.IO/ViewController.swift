add75091f9f999b297dc15955c5f72fd7667905a
JkyProgrammer <jake.costen@dsl.pipex.com>
2018-06-23 11:27:03 +0100

    Initial Commit

---
+++ Doc.IO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Doc.IO
+//
+//  Created by Javax on 23/06/2018.
+//  Copyright © 2018 Javax Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
